ea86583f20d294a96511c62c60b2d1371791c200
solsikke74 <live.cornelia.gravdal@vlfk.no>
2026-01-08 09:09:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ console.log(tall);
 var tekst = ('Jeg håper du har en fin dag');
 console.log(tekst);
 
+var tall1 = 25;
 
+if(tall1 === 5) {
+    console.log("Tallet er 5");
+} else {
+    console.log("Tallet er ikke 5");
+}
+
+var alderKjoring = 0;
+
+if(alderKjoring >= 18) {
+    console.log("Du er gammel nok til å kjøre bil");
+} else if(alderKjoring >= 16) {
+    console.log("Du er gammel nok til å kjøre moped");
+} else {
+    console.log("Du er dessverre ikke gammel nok");
+}
