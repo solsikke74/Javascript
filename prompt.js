702058d08923e1f6c2d0c86e789cb8e7a7c79690
solsikke74 <117269718+solsikke74@users.noreply.github.com>
2026-01-12 10:35:38 +0100

    Update prompt.js

--- prompt.js
+++ prompt.js
@@ -1,5 +1,2 @@
-var alder = prompt("Hvor gammel er du?");
-var dato = new Date(); // Henter informasjon om dagen i dag. 
-var tid = dato.getHours(); //Henter timen (klokka) vi er i nå. 
 
-console.log(tid);
+
