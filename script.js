a562e08c50120523abcc3c158150c18998c64f7d
solsikke74 <117269718+solsikke74@users.noreply.github.com>
2026-01-12 09:06:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 var tall = 9;
 console.log(tall);
 =======
+=======
+>>>>>>> Stashed changes
 var tekst = "Hei Sveinung, jeg håper du har en fin dag :)";
 var tall = 10;
 var tall1 = 40;
 var tall2 = 20;
 var tall3 = tall2; 
+<<<<<<< Updated upstream
+=======
+
+console.log(tekst);
+console.log(tall);
+console.log(tall1+tall2);
+console.log(tall1+tall3);
+
+if(tall === 5){
+    console.log("Tallet er 5");
+} else {
+    console.log("Tallet er ikke 5");
+}
+
+var alder = 16;
+
+if (alder>=18) {
+    console.log("Du er gammel nok til å kjøre bil!");
+} else if (alder >= 16){
+    console.log("Du er gammel nok til å kjøre moped");
+} else{
+    console.log("Du er dessverre ikke gammel nok");
+}
+
+var alder = prompt ("Hvor gammel er du?");
+
+var dato = new Date();
+var mnd = date.GetMonth();
+var dato = date.Gethours();
+>>>>>>> Stashed changes
 
 console.log(tekst);
 console.log(tall);
