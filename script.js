e44895d08255980ac202033e8eadb22b97fc2f2e
solsikke74 <117269718+solsikke74@users.noreply.github.com>
2026-01-12 09:30:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 var tall = 9;
 console.log(tall);
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
 var tekst = "Hei Sveinung, jeg håper du har en fin dag :)";
 var tall = 10;
 var tall1 = 40;
 var tall2 = 20;
 var tall3 = tall2; 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
-=======
 
 console.log(tekst);
 console.log(tall);
@@ -44,114 +28,4 @@ if (alder>=18) {
     console.log("Du er dessverre ikke gammel nok");
 }
 
-var alder = prompt ("Hvor gammel er du?");
 
-var dato = new Date();
-var mnd = date.GetMonth();
-var dato = date.Gethours();
->>>>>>> Stashed changes
-
-console.log(tekst);
-console.log(tall);
-console.log(tall1+tall2);
-console.log(tall1+tall3);
-
-if(tall === 5){
-    console.log("Tallet er 5");
-} else {
-    console.log("Tallet er ikke 5");
-}
-
-var alder = 16;
-
-if (alder>=18) {
-    console.log("Du er gammel nok til å kjøre bil!");
-} else if (alder >= 16){
-    console.log("Du er gammel nok til å kjøre moped");
-} else{
-    console.log("Du er dessverre ikke gammel nok");
-}
-
-var alder = prompt ("Hvor gammel er du?");
-
-var dato = new Date();
-var mnd = date.GetMonth();
-var dato = date.Gethours();
->>>>>>> Stashed changes
-
-console.log(tekst);
-console.log(tall);
-console.log(tall1+tall2);
-console.log(tall1+tall3);
-
-if(tall === 5){
-    console.log("Tallet er 5");
-} else {
-    console.log("Tallet er ikke 5");
-}
-
-var alder = 16;
-
-if (alder>=18) {
-    console.log("Du er gammel nok til å kjøre bil!");
-} else if (alder >= 16){
-    console.log("Du er gammel nok til å kjøre moped");
-} else{
-    console.log("Du er dessverre ikke gammel nok");
-}
-
-var alder = prompt ("Hvor gammel er du?");
-
-var dato = new Date();
-var mnd = date.GetMonth();
-var dato = date.Gethours();
->>>>>>> Stashed changes
-
-console.log(tekst);
-console.log(tall);
-console.log(tall1+tall2);
-console.log(tall1+tall3);
-
-if(tall === 5){
-    console.log("Tallet er 5");
-} else {
-    console.log("Tallet er ikke 5");
-}
-
-var alder = 16;
-
-if (alder>=18) {
-    console.log("Du er gammel nok til å kjøre bil!");
-} else if (alder >= 16){
-    console.log("Du er gammel nok til å kjøre moped");
-} else{
-    console.log("Du er dessverre ikke gammel nok");
-}
-
-var alder = prompt ("Hvor gammel er du?");
-
-var dato = new Date();
-var mnd = date.GetMonth();
-var dato = date.Gethours();
->>>>>>> Stashed changes
-
-var tekst = ('Jeg håper du har en fin dag');
-console.log(tekst);
-
-var tall1 = 25;
-
-if(tall1 === 5) {
-    console.log("Tallet er 5");
-} else {
-    console.log("Tallet er ikke 5");
-}
-
-var alderKjoring = 0;
-
-if(alderKjoring >= 18) {
-    console.log("Du er gammel nok til å kjøre bil");
-} else if(alderKjoring >= 16) {
-    console.log("Du er gammel nok til å kjøre moped");
-} else {
-    console.log("Du er dessverre ikke gammel nok");
-}
