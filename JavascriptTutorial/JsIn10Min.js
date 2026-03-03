50ad8a6eb79de574a2cef1b81c986605c2f764ec
solsikke74 <live.cornelia.gravdal@vlfk.no>
2026-03-03 12:26:08 +0100

    2026.3.3.Mid Day

---
+++ JavascriptTutorial/JsIn10Min.js
@@ -0,0 +1,6 @@
+var button = document.querySelector(".button");
+var box =document.getElementById("ChangeColor");
+
+button.onclick = function ChangeColor() {
+    box.style.background = "#778DA9";
+}   
