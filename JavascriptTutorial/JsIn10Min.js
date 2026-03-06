2de1aa65020f1ad11672a05e32e9e8f36b663cf0
solsikke74 <live.cornelia.gravdal@vlfk.no>
2026-03-06 18:18:48 +0100

    Update JsIn10Min.js

--- JavascriptTutorial/JsIn10Min.js
+++ JavascriptTutorial/JsIn10Min.js
@@ -2,9 +2,9 @@ var button = document.querySelector("button");
 var box = document.getElementById("ChangeMe");
 
 function ChangeColor() {
-    if (box.style.background == "rgb(119, 141, 169)") {
-        box.style.background = "#0D1B2A";
-    }else {
+    if (box.style.background == "rgb(119, 141, 169)") { /*HEX fungerer helt fint i JS. */
+        box.style.background = "#0D1B2A";               /*Problem oppstår bare når du prøver å lese fargen tilbake.*/
+    }else {                                               /*  CSS → JS gir alltid RGB, og en bestemt RGB.*/
         box.style.background = "#778DA9";
     }
-}
+}                                                       
