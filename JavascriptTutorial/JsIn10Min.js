2dd2962466d0f13386120d09d6fb099c1af7749f
solsikke74 <live.cornelia.gravdal@vlfk.no>
2026-03-06 18:07:15 +0100

    260603

--- JavascriptTutorial/JsIn10Min.js
+++ JavascriptTutorial/JsIn10Min.js
@@ -2,9 +2,9 @@ var button = document.querySelector("button");
 var box = document.getElementById("ChangeMe");
 
 function ChangeColor() {
-    if (box.style.background == "red") {
-        box.style.background = "#415A77";
+    if (box.style.background == "rgb(119, 141, 169)") {
+        box.style.background = "#0D1B2A";
     }else {
-        box.style.background = "red";
+        box.style.background = "#778DA9";
     }
 }
