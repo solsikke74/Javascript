733b13a22256cb9b8d5028d52e4ec74f6a1168f3
solsikke74 <live.cornelia.gravdal@vlfk.no>
2026-03-20 18:47:25 +0100

    261803_1

--- Del1/Switch.js
+++ Del1/Switch.js
@@ -2,9 +2,27 @@ var button = document.querySelector("button");
 var box = document.getElementById("ChangeMe");
 
 function ChangeColor() {
-    if (box.style.background == "rgb(119, 141, 169)") { /*HEX fungerer helt fint i JS. */
-        box.style.background = "#0D1B2A";               /*Problem oppstår bare når du prøver å lese fargen tilbake.*/
-    }else {                                               /*  CSS → JS gir alltid RGB, og en bestemt RGB.*/
-        box.style.background = "#778DA9";
+    let current = getComputedStyle(box).backgroundColor;
+
+    switch (current) {  
+        case "rgb(119, 141, 169)":
+            box.style.background = "rgb(13, 27, 42)";
+            break;
+
+        case "rgb(13, 27, 42)":
+            box.style.background = "rgb(27, 38, 59)";
+            break;
+
+        case "rgb(27, 38, 59)":
+            box.style.background = "rgb(224, 225, 221)";
+            break;
+
+        case "rgb(224, 225, 221)":
+            box.style.background = "rgb(65, 90, 119)";
+            break;
+
+        case "rgb(65, 90, 119)":
+            box.style.background = "rgb(119, 141, 169)";
     }
-}                                                       
+}
+                                                     
